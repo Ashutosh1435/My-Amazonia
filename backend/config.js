9a46929f1b366d161d7850a26878393a6a65ef49
Ashutosh Singh <ashutoshsinghchauhan1435@gmail.com>
2020-11-16 21:34:24 +0530

    2nd commit

--- backend/config.js
+++ backend/config.js
@@ -4,11 +4,11 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 export default {
-    port: process.env.PORT,
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID || 'accessKeyId',
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || 'secretAccessKey',
-    sharpPath: process.env.AWS_SHARP_PATH || 'sharpPath',
+    port: process.env.local.PORT,
+    accessKeyId: process.env.local.AWS_ACCESS_KEY_ID || 'accessKeyId',
+    secretAccessKey: process.env.local.AWS_SECRET_ACCESS_KEY || 'secretAccessKey',
+    sharpPath: process.env.local.AWS_SHARP_PATH || 'sharpPath',
 
-    instamojoSandboxKey: process.env.INSTAMOJO_SANDBO_KEY || 'instamojoSandboxKey',
-    instamojoSandboxToken: process.env.INSTAMOJO_SANDBOX_TOKEN || 'instamojoSandboxKey',
+    instamojoSandboxKey: process.env.local.INSTAMOJO_SANDBO_KEY || 'instamojoSandboxKey',
+    instamojoSandboxToken: process.env.local.INSTAMOJO_SANDBOX_TOKEN || 'instamojoSandboxKey',
 }
